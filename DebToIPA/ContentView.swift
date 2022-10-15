45da11725b227bb368c69b5f3b580302fc58b1c8
Matthew <anisovich.matthew@gmail.com>
2022-10-15 11:18:14 +0100

    Support bzip2 & xz + ui updates

--- DebToIPA/ContentView.swift
+++ DebToIPA/ContentView.swift
@@ -10,30 +10,52 @@ import SwiftUI
 struct ContentView: View {
     @State private var isImporting = false
     @State private var alertMessage = ""
+    @State private var alertTitle = ""
     @State private var showingAlert = false
     @State private var converting = false
+    @State private var performCleanup = true
+    @State private var statusText = ""
     
     var body: some View {
         NavigationView {
             VStack {
-                ProgressView()
-                    .padding()
-                    .opacity(converting ? 1 : 0)
-                Button("Import .deb", action: {
+                HStack {
+                    if converting {
+                        ProgressView()
+                            .padding()
+                    }
+                    Text(statusText)
+                        .foregroundColor(.secondary)
+                        .font(.caption)
+                }
+                Button("Convert .deb app", action: {
                     isImporting.toggle()
                 })
-                .padding(10)
-                .background(Color.blue)
-                .cornerRadius(8)
-                .foregroundColor(.white)
-                Text("By sourcelocation\n\nCredits: itsnebulalol,\nLebJe/ArArchiveKit,\nmarmelroy/Zip,\ntsolomko/SWCompression")
-                    .foregroundColor(Color.secondary)
-                    .font(.caption)
-                    .multilineTextAlignment(.center)
-                
+                    .padding(10)
+                    .background(Color.blue)
+                    .cornerRadius(8)
+                    .foregroundColor(.white)
+                HStack(alignment: .center) {
+                    Text("By sourcelocation")
+                        .foregroundColor(.secondary)
+                        .font(.caption)
+                    Button {
+                        alert("\nCredits: itsnebulalol,\nLebJe/ArArchiveKit,\nmarmelroy/Zip,\ntsolomko/SWCompression", title: "Info")
+                    } label: {
+                        Image(systemName: "info.circle")
+                    }
+                }
+                HStack {
+                    Text("Clean after running")
+                        .font(.body)
+                    Toggle("Cleanup", isOn: $performCleanup)
+                        .labelsHidden()
+                }
+                .padding()
             }
             .navigationTitle("DebToIPA")
         }
+        .navigationViewStyle(StackNavigationViewStyle())
         .fileImporter(
             isPresented: $isImporting,
             allowedContentTypes: [.init(filenameExtension: "deb")!],
@@ -44,7 +66,7 @@ struct ContentView: View {
         }
         .alert(isPresented: $showingAlert) {
             Alert(
-                title: Text("Error"),
+                title: Text(alertTitle),
                 message: Text(alertMessage)
             )
         }
@@ -52,36 +74,44 @@ struct ContentView: View {
     
     func convert(url: URL) {
         converting = true
-        DispatchQueue.main.asyncAfter(deadline: .now() + 0.1, execute: {
+        DispatchQueue.global(qos: .userInitiated).async {
             do {
                 guard url.startAccessingSecurityScopedResource() else { throw ConversionError.noPermission }
-                let ipa = try DebToIPA.convert(url)
-                share(ipa)
-            } catch let error {
-                print(error)
-                if let convError = error as? ConversionError {
-                    switch convError {
-                    case .unsupportedApp:
-                        alert("The .deb you imported is UNSUPPORTED and CANNOT be converted to .ipa, as it doesn't have Applications folder.")
-                    case .noDataFound:
-                        alert("Data wasn't found in .deb. Are you sure the .deb you imported isn't corrupted?")
-                    case .noApplication:
-                        alert("The .deb you imported is UNSUPPORTED and CANNOT be converted to .ipa, as it doesn't have a .app file inside it.")
-                    case .noPermission:
-                        alert("No permission to view the file")
-                    case .unknownFiletypeInsideTar:
-                        alert("Unknown filetype inside tar. This is a bug with the app. Please create an issue on Github with the name of tweak/app. Thanks!")
-                    case .unsupportedCompression:
-                        alert("Unsupported compression. This is a bug with the app. Please create an issue on Github with the name of tweak/app. Thanks!")
+                let ipa = try DebToIPA.convert(url, statusUpdate: { message in
+                    DispatchQueue.main.async {
+                        statusText = message
                     }
-                } else {
-                    alert("Unknown error.\n" + error.localizedDescription)
+                })
+                DispatchQueue.main.async {
+                    share(ipa)
+                }
+            } catch let error {
+                DispatchQueue.main.async {
+                    print(error)
+                    if let convError = error as? ConversionError {
+                        switch convError {
+                        case .unsupportedApp:
+                            alert("The .deb you imported is UNSUPPORTED and CANNOT be converted to .ipa, as it doesn't have Applications folder.")
+                        case .noDataFound:
+                            alert("Data wasn't found in .deb. Are you sure the .deb you imported isn't corrupted?")
+                        case .noApplication:
+                            alert("The .deb you imported is UNSUPPORTED and CANNOT be converted to .ipa, as it doesn't have a .app file inside it.")
+                        case .noPermission:
+                            alert("No permission to view the file")
+                        case .unknownFiletypeInsideTar:
+                            alert("Unknown filetype inside tar. This is a bug with the app. Please create an issue on Github with the name of tweak/app. Thanks!")
+                        case .unsupportedCompression:
+                            alert("Unsupported compression. This is a bug with the app. Please create an issue on Github with the name of tweak/app. Thanks!")
+                        }
+                    } else {
+                        alert("Unknown error.\n" + error.localizedDescription)
+                    }
                 }
             }
             url.stopAccessingSecurityScopedResource()
             
             converting = false
-        })
+        }
     }
     
     func share(_ url: URL) {
@@ -91,14 +121,23 @@ struct ContentView: View {
             shareActivity.popoverPresentationController?.sourceView = vc.view
             shareActivity.popoverPresentationController?.sourceRect = CGRect(x: UIScreen.main.bounds.width / 2, y: UIScreen.main.bounds.height, width: 0, height: 0)
             shareActivity.popoverPresentationController?.permittedArrowDirections = UIPopoverArrowDirection.down
+            shareActivity.completionWithItemsHandler = { (type,completed, returnedItems, activityError) in
+                if performCleanup {
+                    do {
+                        try DebToIPA.cleanup()
+                    } catch {
+                        alert("Cleanup failed. Reinstall is most likely needed. " + error.localizedDescription)
+                    }
+                }
+            }
             DispatchQueue.main.async {
-                vc.present(shareActivity, animated: true, completion: {
-                })
+                vc.present(shareActivity, animated: true)
             }
         }
     }
     
-    func alert(_ message: String) {
+    func alert(_ message: String, title: String = "Error") {
+        alertTitle = title
         alertMessage = message
         showingAlert.toggle()
     }
--- DebToIPA/DebToIPA.swift
+++ DebToIPA/DebToIPA.swift
@@ -15,13 +15,14 @@ class DebToIPA {
     private static var tempDir: URL { fm.temporaryDirectory }
     
     /// Converts .deb app to .ipa, returns url of .ipa
-    static func convert(_ url: URL) throws -> URL {
+    static func convert(_ url: URL, statusUpdate: (String) -> ()) throws -> URL {
         try cleanup()
         let payloadDir = tempDir.appendingPathComponent("Payload")
         
         // Extract .deb
-        let appURLs = try extractDeb(url)
+        let appURLs = try extractDeb(url, statusUpdate: statusUpdate)
         
+        statusUpdate("Creating .ipa archive")
         // Create .ipa archive
         try fm.createDirectory(at: payloadDir, withIntermediateDirectories: true)
         for url in appURLs {
@@ -29,21 +30,25 @@ class DebToIPA {
         }
         
         // Create archive of ipa folder
+        statusUpdate("Zipping app")
         let zipFilePath = try Zip.quickZipFiles([payloadDir], fileName: url.deletingPathExtension().lastPathComponent) // Zip
         
         // Rename
+        statusUpdate("Renaming .zip to .ipa")
         let destIpaURL = zipFilePath.deletingPathExtension().appendingPathExtension("ipa")
         try? fm.removeItem(at: destIpaURL)
         try fm.moveItem(at: zipFilePath, to: destIpaURL)
         
+        statusUpdate("Done.")
         return zipFilePath.deletingPathExtension().appendingPathExtension("ipa")
     }
     
     
     /// Extracts deb and returns .app urls
-    static func extractDeb(_ url: URL) throws -> [URL] {
+    static func extractDeb(_ url: URL, statusUpdate: (String) -> ()) throws -> [URL] {
         let extractedDir = tempDir.appendingPathComponent("extracted")
         let appsDir = tempDir.appendingPathComponent( "extracted/Applications/")
+        statusUpdate("Reading .deb")
         let reader = try ArArchiveReader(archive: Array<UInt8>(Data(contentsOf: url)))
         var foundData = false
         for (header, dataInts) in reader {
@@ -51,24 +56,38 @@ class DebToIPA {
             let dataURL = tempDir.appendingPathComponent(header.name)
             
             // Write data to disk
+            statusUpdate("Creating data from ints")
             let data = Data(dataInts)
             try data.write(to: dataURL, options: .atomic)
             
+            statusUpdate("Decompressing data archive")
             let decompressedData: Data?
             switch DecompressionMethod(rawValue: header.name.components(separatedBy: ".").last ?? "") {
             case .lzma:
                 foundData = true
+                statusUpdate("Using LZMA")
                 decompressedData = try LZMA.decompress(data: data)
             case .gz:
                 foundData = true
+                statusUpdate("Using GzipArchive")
                 decompressedData = try GzipArchive.unarchive(archive:data)
+            case .bzip2:
+                foundData = true
+                statusUpdate("Using BZip2")
+                decompressedData = try BZip2.decompress(data:data)
+            case .xz:
+                foundData = true
+                statusUpdate("Using XZArchive")
+                decompressedData = try XZArchive.unarchive(archive:data)
             case .none:
                 throw ConversionError.unsupportedCompression
             }
             
+            statusUpdate("Opening .tar")
             try decompressedData!.write(to: extractedDir.appendingPathExtension("tar"))
             let tarContainer = try TarContainer.open(container: decompressedData!)
             
+            statusUpdate("Creating files")
             for entry in tarContainer {
                 if entry.info.type == .directory {
                     try fm.createDirectory(at: extractedDir.appendingPathComponent(entry.info.name), withIntermediateDirectories: true)
@@ -96,7 +115,7 @@ class DebToIPA {
 }
 
 enum DecompressionMethod: String {
-    case gz, lzma // todo
+    case gz, lzma, bzip2, xz
 }
 
 enum ConversionError: Error {
